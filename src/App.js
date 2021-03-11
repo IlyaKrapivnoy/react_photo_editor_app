4f6075914606f1e4fa4dab94b6ec6e994fa54055
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-11 18:18:54 +0200

    SidebarItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Slider from './Slider'
+import SidebarItem from './SidebarItem'
 
 function App() {
   return (
     <div className="container">
        <div className="main-image"></div>
        <div className="sidebar">
-        Sidebar
+        <SidebarItem />
+        <SidebarItem />
        </div>
        <Slider />
     </div>
---
+++ src/SidebarItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SidebarItem = () => {
+    return (
+        <button 
+        className="sidebar-item"
+        >
+            Sidebar Item
+        </button>
+    )
+}
+
+export default SidebarItem
